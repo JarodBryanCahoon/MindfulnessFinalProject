a76d20b9ac0a666eb0572878bb9e6eb0f98e81eb
Jarod Cahoon <jbc53@duke.edu>
2018-04-06 20:19:40 -0400

    Added in more functionality to my navbar and whatnot

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
 </template>
 
 <script>
+  import NavBar from '../components/navbar'
+  import Gallery from '../components/gallery'
+  import Biblio from '../components/bibliography'
+  import About from '../components/about'
+  
   export default {
     data () {
       return {
---
+++ src/components/navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <v-toolbar>
+            <v-menu>
+                <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+                <v-list>
+                    <v-btn>
+                        About
+                    </v-btn>
+                    <v-btn>
+                        Gallery
+                    </v-btn>
+                    <v-btn>
+                        Bibliography
+                    </v-btn>
+                </v-list>
+            </v-menu>
+            <v-toolbar-title>A Mindful Gallery</v-toolbar-title>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                currentSite : null,
+            }
+        }
+    }
+</script>
