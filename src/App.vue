0c9a26bd0a57322e99dbcdc2621662a55902bdea
Jarod Cahoon <jbc53@duke.edu>
2018-04-06 20:26:28 -0400

    Kind of hooked stuff up, not sure if it works

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
-
+    <NavBar></NavBar>
+    <Gallery v-if="this.currentSite == Gallery"></Gallery>
+    <Biblio v-if="this.currentSite == Biblio"></Biblio>
+    <About v-if="this.currentSite == About"></About>
   </div>
 </template>
 
 <script>
-  import NavBar from '../components/navbar'
-  import Gallery from '../components/gallery'
-  import Biblio from '../components/bibliography'
-  import About from '../components/about'
-  
-  export default {
-    data () {
-      return {
-         currentSite : null,
+    import NavBar from '../components/navbar'
+    import Gallery from '../components/gallery'
+    import Biblio from '../components/bibliography'
+    import About from '../components/about'
 
+    export default {
+        data () {
+            return {
+                currentSite : About,
+            }
+        },
 
-      }
+        components: {
+            NavBar,
+            Gallery,
+            Biblio,
+            About
+        },
+
+        methods : {
+
+        }
     }
-  }
 </script>
