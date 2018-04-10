8c474ff663264b543eb1725ce7a34c54ef5a18e6
Jarod Cahoon <jbc53@duke.edu>
2018-04-10 10:58:32 -0400

    Added some shit to the about profile

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,6 +1,26 @@
 <template>
-    <v-parallax src="src/images/mountain2.jpg">
-
+    <v-parallax src="src/images/mountain2.jpg" height="800">
+        <v-container align-center>
+            <v-card color="grey">
+                <v-card-text display-3>
+                    &nbsp&nbsp&nbsp&nbsp For this project, I wanted to provide more of a look at more of the "hard science"
+                    behind meditation, and juxtapose it with pictures of how these benefits of meditation are displayed artistically.
+                    From the outside, meditation can seem to be shrouded in mysticism and practiced exclusively by holy men
+                    or ascetics who dedicate their entire lives to the cause. This cannot be further from the truth, but
+                    it can be easy to understand why some might think this. From this perspective, it can be difficult for a
+                    beginner to see the true value and meditation and mindfulness, and seem daunting to break into the practice
+                    at all.
+                </v-card-text>
+                <v-card-text>
+                    &nbsp&nbsp&nbsp&nbsp I believe that a good way to combat this "stigma" and made mindfulness more accessible
+                    is to be up front with the health benefits that these practices provide, in addition to having visual representations
+                    of how these certain characteristics may make you feel.
+                </v-card-text>
+                <v-card-text>
+                    MORE TEXT TO COME!!!!!!!!!!!!!
+                </v-card-text>
+            </v-card>
+        </v-container>
     </v-parallax>
 </template>
 
--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -1,13 +1,7 @@
 <template>
-<<<<<<< HEAD
-    <div>
-       <v-parallax src="../images/mountains.jpg"></v-parallax>
-    </div>
-=======
        <v-parallax src="src/images/mountains.jpg" height="800">
 
        </v-parallax>
->>>>>>> work
 </template>
 
 <script>
